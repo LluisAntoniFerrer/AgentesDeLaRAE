2dedca71150eb611e8e93ba825663fa3e7d5f9a3
PClLuis <PCLluis@DESKTOP-EQC35FF>
2016-07-01 11:46:32 +0200

    Cambiada la pantalla finl

--- AgentesDeLaRAE/src/vista/Respuesta.java
+++ AgentesDeLaRAE/src/vista/Respuesta.java
@@ -9,8 +9,14 @@ import javax.swing.JTextArea;
 import java.awt.SystemColor;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.LineBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Respuesta extends JPanel {
+	private JTable table;
 
 	/**
 	 * Create the panel.
@@ -21,43 +27,81 @@ public class Respuesta extends JPanel {
 		setLayout(null);
 		setBounds(0,0,800,640);
 		
-		JButton btnCompruebaTusResultados = new JButton("Comprueba tus resultados");
-		btnCompruebaTusResultados.setBounds(521, 530, 184, 23);
-		add(btnCompruebaTusResultados);
+		JButton btnMenu = new JButton("Menu Principal");
+		btnMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				framePrincipal.CambiaFondo("inicial");
+			}
+		});
+		btnMenu.setBounds(511, 524, 194, 29);
+		add(btnMenu);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(Respuesta.class.getResource("/images/barra.png")));
-		label.setBounds(250, 107, 264, 30);
-		add(label);
+		JButton btnReiniciarNivel = new JButton("Reiniciar nivel");
+		btnReiniciarNivel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				framePrincipal.CambiaFondo("pregunta1");
+			}
+		});
+		btnReiniciarNivel.setBounds(511, 479, 194, 29);
+		add(btnReiniciarNivel);
 		
-		JLabel lblAhoraCompruebaTus = new JLabel("Ahora comprueba");
+		table = new JTable();
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"1", "0%"},
+				{"2", "0%"},
+				{"3", "0%"},
+				{"4", "0%"},
+				{"5", "0%"},
+				{"6", "0%"},
+				{"7", "0%"},
+				{"8", "0%"},
+				{"9", "0%"},
+				{"10", "0%"},
+			},
+			new String[] {
+				"Nivel", "Aciertos"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.setBounds(483, 127, 222, 267);
+		add(table);
+		
+		JLabel lblAhoraCompruebaTus = new JLabel("\u00BFVolvemos a");
 		lblAhoraCompruebaTus.setFont(new Font("Segoe Print", Font.BOLD, 15));
-		lblAhoraCompruebaTus.setBounds(437, 211, 161, 14);
+		lblAhoraCompruebaTus.setBounds(188, 192, 161, 23);
 		add(lblAhoraCompruebaTus);
 		
-		JLabel lblBienHecho = new JLabel("\u00A1 Bien hecho!");
+		JLabel lblBienHecho = new JLabel("\u00A1 Necesitas mejorar!");
 		lblBienHecho.setFont(new Font("Segoe Print", Font.BOLD, 15));
-		lblBienHecho.setBounds(451, 177, 147, 23);
+		lblBienHecho.setBounds(178, 153, 170, 23);
 		add(lblBienHecho);
 		
-		JLabel lblTusResultados = new JLabel("tus resultados...");
+		JLabel lblTusResultados = new JLabel("intentarlo?");
 		lblTusResultados.setFont(new Font("Segoe Print", Font.BOLD, 15));
-		lblTusResultados.setBounds(437, 231, 161, 14);
+		lblTusResultados.setBounds(188, 226, 135, 14);
 		add(lblTusResultados);
 		
 		JLabel label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon(Respuesta.class.getResource("/images/bocadillo_.png")));
-		label_2.setBounds(415, 162, 238, 164);
+		label_2.setIcon(new ImageIcon(Respuesta.class.getResource("/images/bocadillo.png")));
+		label_2.setBounds(111, 127, 288, 176);
 		add(label_2);
 		
 		JLabel label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon(Respuesta.class.getResource("/images/RAE2.jpg")));
-		label_1.setBounds(136, 304, 356, 249);
+		label_1.setIcon(new ImageIcon(Respuesta.class.getResource("/images/agentes.png")));
+		label_1.setBounds(91, 304, 356, 249);
 		add(label_1);
 		JLabel fondo = new JLabel("");
 		fondo.setIcon(new ImageIcon(Respuesta.class.getResource("/images/PAPIRO 8.png")));
 		fondo.setBounds(0, 0, 800, 640);
 		add(fondo);
 	}
-
 }
